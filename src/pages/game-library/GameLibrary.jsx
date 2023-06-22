4bce0dd330cee07dd10e595c65639df02661e66b
lmluat <lmluat99@gmail.com>
2023-06-22 15:01:53 +0700

    finish game library of user page

--- src/pages/game-library/GameLibrary.jsx
+++ src/pages/game-library/GameLibrary.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { fetchGameLibraryApi } from "src/services/game";
 
 export default function GameLibrary() {
-  return (
-    <div>Game Library</div>
-  )
+  const [gameLibrary, setGameLibrary] = useState([]);
+
+  const fetchGameLibrary = useCallback(async () => {
+    const result = await fetchGameLibraryApi();
+    console.log(result);
+
+    setGameLibrary(result.data);
+  }, []);
+
+  console.log(gameLibrary);
+
+  useEffect(() => {
+    fetchGameLibrary();
+  }, [fetchGameLibrary]);
+
+  const renderGameLibrary = gameLibrary.map((game,index) => {
+    return (
+      <div key={index+1}>
+        <h1>Game Name: {game.gameName}</h1>
+        <p>Game price: {game.gamePrice}</p>
+        <p>Date: {game.receiptDate}</p>
+      </div>
+    );
+  });
+
+  return <div>{renderGameLibrary}</div>;
 }
